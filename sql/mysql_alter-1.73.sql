19437f8bb988e2787d14b85f365a7a99cdb6644c
MrTwinkles47 <mrtwinkles47@gmail.com>
2024-09-01 19:58:12 -0400

    merge 1.73 alters

--- sql/mysql_alter-1.73.sql
+++ sql/mysql_alter-1.73.sql
@@ -1,5 +1,20 @@
 --SQL changes for upgrades from 1.71/1.72 to 1.73:
 
+-- From mysql_Alter-1.7x.sql
+ALTER TABLE `sm_scores` CHANGE `survive_seconds` `survive_seconds` DECIMAL(12,6) NULL DEFAULT NULL;
+ALTER TABLE `sm_scores` CHANGE `life_remaining_seconds` `life_remaining_seconds` DECIMAL(12,6) NULL DEFAULT NULL;
+ALTER TABLE `sm_scores` CHANGE `w1` `w1` SMALLINT(6) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `sm_scores` CHANGE `w2` `w2` SMALLINT(6) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `sm_scores` CHANGE `w3` `w3` SMALLINT(6) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `sm_scores` CHANGE `w4` `w4` SMALLINT(6) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `sm_scores` CHANGE `w5` `w5` SMALLINT(6) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `sm_scores` CHANGE `miss` `miss` SMALLINT(6) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `sm_scores` CHANGE `held` `held` SMALLINT(6) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `sm_scores` CHANGE `notes` `notes` SMALLINT(6) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `sm_scores` CHANGE `mines` `mines` SMALLINT(6) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `sm_scores` CHANGE `taps_holds` `taps_holds` SMALLINT(6) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `sm_scores` CHANGE `max_combo` `max_combo` INT(10) NULL DEFAULT NULL;
+
 --Add Webhooks table
 CREATE TABLE `sm_webhooks` (
   `id` int(11) NOT NULL,
